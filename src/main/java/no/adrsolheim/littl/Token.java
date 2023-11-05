b763cf5addf9163833e94faf54f20ed06c31c3da
Adrian Solheim <adrsolheim@gmail.com>
2023-11-05 17:04:13 +0100

    Introduce Token and TokenTypes

---
+++ src/main/java/no/adrsolheim/littl/Token.java
@@ -0,0 +1,19 @@
+package no.adrsolheim.littl;
+
+public class Token {
+    private final TokenType tokenType;
+    private final String lexeme;
+    private final Object literal;
+    private final int line;
+
+    public Token(TokenType tokenType, String lexeme, Object literal, int line) {
+        this.tokenType = tokenType;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return STR."\{tokenType} \{lexeme} \{literal}";
+    }
+}
---
+++ src/main/java/no/adrsolheim/littl/TokenType.java
@@ -0,0 +1,23 @@
+package no.adrsolheim.littl;
+
+public enum TokenType {
+    // single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    PLUS, MINUS, SLASH, STAR,
+    COMMA, DOT,  SEMICOLON,
+
+    // one or two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    IF, ELSE, AND, OR,
+    FUN, RETURN, VAR, NIL,
+    WHILE, FOR, THIS, PRINT,
+    CLASS, SUPER,
+
+    EOF
+}
