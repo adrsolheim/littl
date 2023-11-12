e6b8c23378a3b1b59e2c75d0c477660244f9cbe2
Adrian Solheim <adrsolheim@gmail.com>
2023-11-12 17:40:43 +0100

    Add AST string printer (visitor)

---
+++ src/main/java/no/adrsolheim/littl/AstPrinter.java
@@ -0,0 +1,39 @@
+package no.adrsolheim.littl;
+
+public class AstPrinter implements Expr.Visitor<String> {
+
+    public void print(Expr expr) {
+        System.out.println(expr.accept(this));
+    }
+
+    @Override
+    public String visit(Expr.Literal expr) {
+        return expr.value == null ? "nil" : String.valueOf(expr.value);
+    }
+
+    @Override
+    public String visit(Expr.Binary expr) {
+        return parensWrap(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visit(Expr.Unary expr) {
+        return expr.operator.lexeme + expr.expr.accept(this);
+    }
+
+    @Override
+    public String visit(Expr.Grouping expr) {
+        return parensWrap("grouping", expr.expr);
+    }
+
+    String parensWrap(String name, Expr... exprs) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append("(");
+        for (Expr expr : exprs) {
+            sb.append(expr.accept(this));
+        }
+        sb.append(") ");
+        return sb.toString();
+    }
+}
