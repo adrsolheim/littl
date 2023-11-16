53b937ea49cb99746dd94b08ae4bb72c51fdfece
Adrian Solheim <adrsolheim@gmail.com>
2023-11-16 21:09:53 +0100

    Add grouping, unary and literal expression implementations

--- src/main/java/no/adrsolheim/littl/Parser.java
+++ src/main/java/no/adrsolheim/littl/Parser.java
@@ -59,13 +59,14 @@ public class Parser {
         while(match(TokenType.LESS, TokenType.LESS_EQUAL, TokenType.GREATER, TokenType.GREATER_EQUAL)) {
             Token operator = previous();
             Expr right = term();
-            expr = new Expr.Binary(expr, operator right);
+            expr = new Expr.Binary(expr, operator, right);
         }
+        return expr;
     }
 
     private Expr term() {
         Expr expr = factor();
-        while(match(TokenType.MINUS, TokenType.PLUS, TokenType.SLASH, TokenType.STAR)) {
+        while(match(TokenType.MINUS, TokenType.PLUS)) {
             Token operator = previous();
             Expr right = factor();
             expr = new Expr.Binary(expr, operator, right);
@@ -74,6 +75,47 @@ public class Parser {
     }
 
     private Expr factor() {
+        Expr expr = unary();
+        while(match(TokenType.SLASH, TokenType.STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(TokenType.BANG,TokenType.MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+        return literal();
+    }
+
+    private Expr literal() {
+        if (match(TokenType.TRUE)) return new Expr.Literal(true);
+        if (match(TokenType.FALSE)) return new Expr.Literal(false);
+        if (match(TokenType.NIL)) return new Expr.Literal(null);
+        if (match(TokenType.NUMBER, TokenType.STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+        if(match(TokenType.LEFT_PAREN)) {
+            consume(TokenType.LEFT_PAREN, "Expected closing ')'");
+            return new Expr.Grouping(expression());
+        }
         return null;
+    }
+
+    private void consume(TokenType expected, String message) {
+        if (expected == TokenType.LEFT_PAREN) {
+            List<TokenType> illegalTypes = List.of(TokenType.EOF, TokenType.RIGHT_BRACE, TokenType.LEFT_PAREN, TokenType.LEFT_BRACE);
+            for (int i = current;;i++) {
+                if (illegalTypes.contains(tokens.get(i))){
+                    System.out.println(message);
+                    break;
+                }
+            }
+        }
     }
 }
