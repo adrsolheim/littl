cd4964c1be82bcaecf0d3f69f80696cbf133ba4f
Adrian Solheim <adrsolheim@gmail.com>
2023-11-18 14:26:59 +0100

    Subset of TokenTypes can be start of a statement

--- src/main/java/no/adrsolheim/littl/TokenType.java
+++ src/main/java/no/adrsolheim/littl/TokenType.java
@@ -2,22 +2,32 @@ package no.adrsolheim.littl;
 
 public enum TokenType {
     // single-character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    PLUS, MINUS, SLASH, STAR,
-    COMMA, DOT,  SEMICOLON,
+    LEFT_PAREN(false), RIGHT_PAREN(false), LEFT_BRACE(false), RIGHT_BRACE(false),
+    PLUS(false), MINUS(false), SLASH(false), STAR(false),
+    COMMA(false), DOT(false),  SEMICOLON(false),
 
     // one or two character tokens
-    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+    BANG(false), BANG_EQUAL(false), EQUAL(false), EQUAL_EQUAL(false),
+    GREATER(false), GREATER_EQUAL(false), LESS(false), LESS_EQUAL(false),
 
     // literals
-    IDENTIFIER, STRING, NUMBER,
+    IDENTIFIER(false), STRING(false), NUMBER(false),
 
     // keywords
-    IF, ELSE, AND, OR,
-    FUN, RETURN, VAR, NIL,
-    WHILE, FOR, THIS, PRINT,
-    CLASS, SUPER, TRUE, FALSE,
+    IF(true), ELSE(false), AND(false), OR(false),
+    FUN(true), RETURN(true), VAR(true), NIL(false),
+    WHILE(true), FOR(true), THIS(false), PRINT(true),
+    CLASS(true), SUPER(true), TRUE(false), FALSE(false),
 
-    SKIP, INVALID, EOF
+    SKIP(false), INVALID(false), EOF(false);
+
+    boolean statementStart;
+
+    TokenType(boolean statementStart) {
+        this.statementStart = statementStart;
+    }
+
+    boolean isStartOfStatement() {
+        return statementStart;
+    }
 }
